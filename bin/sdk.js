ff1f8c7dfef583569dd9107f6debbccde066fb42
Hank <jxhan@huikedu.com>
2020-06-03 14:11:31 +0800

    fix: 修改发布

--- bin/sdk.js
+++ bin/sdk.js
@@ -1,8 +1,32 @@
-#! /usr/bin/env node 
-let commander = require('commander').program
+#! /usr/bin/env node
+let commander = require('commander').program;
+let shelljs = require('shelljs');
+let path = require('path');
+let fs = require('fs');
+let chalk = require('chalk');
+commander.version('1.0.0');
 
-commander.version('1.0.0')
-commander.command('new  <name>').description('创建你的项目').action((name, destination) => {
-  console.log(`new ${name}`);
-})
+const projectTemplate = path.resolve(__dirname, '../packages/project-template/');
+commander
+  .command('new  <name>')
+  .description('创建你的项目')
+  .action((name, destination) => {
+    const resultPath = path.resolve(process.cwd(), name);
+    const isExist = fs.existsSync(resultPath);
+    if (isExist) {
+      console.error(
+        chalk.bgRed(`文件夹已存在换个名字试试吧`),
+      );
+    } else {
+      shelljs.cp('-rf', projectTemplate, name);
+      shelljs.cd(resultPath);
+      shelljs.rm('-rf', '.git');
+      console.log(
+        chalk.green(`
+      创建 ${name} 完成
+      run cd ${name} && npm install
+    `),
+      );
+    }
+  });
 commander.parse(process.argv);
